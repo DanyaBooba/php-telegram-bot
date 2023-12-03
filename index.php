da7dbb4cb1181c41eddf4231136d2dfc207fd44a
Daniil Dybka <creagoooff@gmail.com>
2023-12-03 15:45:12 +0300

    Edit index

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 ## Author: Daniil Dybka, daniil@dybka.ru
 ##
 
+##
+## Init
+##
+
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -13,9 +17,36 @@ ini_set('display_startup_errors', 1);
 include "token.php";
 include "methods.php";
 
+##
+## Message
+##
+
 $token = Token()["token_bot"];
-$chat_id = Token()["admin_id"];
 
-$message = urlencode("Простое текстовое сообщение.");
+$get = [
+    "chat_id" => Token()["admin_id"],
+    "text" => "Новое сообщение пользователю.",
+    "parse_mode" => "html"
+];
 
-$url = Method($token, $chat_id) . $message;
+##
+## Url
+##
+
+$url = curl_init(Method($token)["send_message"] . http_build_query($get));
+
+curl_setopt($url, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($url, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($url, CURLOPT_HEADER, 0);
+
+$result = curl_exec($url);
+
+curl_close($url);
+
+##
+## Json
+##
+
+$json = json_decode($result, 1);
+
+var_dump($json["result"]["chat"]["id"]);
--- methods.php
+++ methods.php
@@ -2,13 +2,13 @@
 
 $main_url = "https://api.telegram.org/bot";
 
-function Method($token, $chat_id = "")
+function Method($token)
 {
     global $main_url;
 
     return [
         "get_updates" => $main_url . $token . "/getUpdates",
-        "send_message" => $main_url . $token . "/sendMessage?chat_id=$chat_id&text=",
+        "send_message" => $main_url . $token . "/sendMessage?",
         "send_document" => $main_url . $token . "/sendDocument",
         "send_photo" => $main_url . $token . "/sendPhoto",
     ];
